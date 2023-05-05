b3f798b0c6ff26767ba2f12a92ccbc6757e8e7d1
radhouene-massoudi <massoudiradhouene@gmail.com>
2023-05-05 16:38:38 +0100

    seance 2 part 2

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,22 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent implements OnInit {
+export class TestComponent implements OnInit, OnChanges {
 klass='1alinfo6'
 kolor='red';
+j=-1;
+d:any;
+search!:number;
 listProdcut=[
   {id: 1, title: "T-shirt 1", price: 18, quantity: 0, like: 0},
   {id: 2, title: "T-shirt 2", price: 21, quantity: 10, like: 0},
   {id: 3, title: "T-shirt 3", price: 16, quantity: 8, like: 0}, ];
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    console.log('cons')
   }
 
+  ngOnInit(): void {
+    console.log('oninit')
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+      
+  }
+  increaseLike(p:any){
+//console.log(p)
+//let index=this.listProdcut.indexOf(p);
+this.listProdcut[p].like++;  
+}
   showMsg(){
     console.log('bonjour')
   }
@@ -26,5 +39,9 @@ listProdcut=[
   changeColor(c:any){
     //console.log(c)
 this.kolor=c
+  }
+  detail(i:any){
+this.d=this.listProdcut[i];
+this.j=i;
   }
 }
